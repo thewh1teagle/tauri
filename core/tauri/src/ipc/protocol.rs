0088bad8530a07e2427d30790f9b204f73aadcab
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-05-02 15:55:53 +0300

    apply patch

--- core/tauri/src/ipc/protocol.rs
+++ core/tauri/src/ipc/protocol.rs
@@ -318,7 +318,7 @@ fn handle_ipc_message<R: Runtime>(request: Request<String>, manager: &AppManager
 
               match &response {
                 InvokeResponse::Ok(InvokeBody::Json(v)) => {
-                  if !(cfg!(target_os = "macos") || cfg!(target_os = "ios"))
+                  if !(cfg!(target_os = "macos") || cfg!(target_os = "ios") || cfg!(target_os = "windows"))
                     && matches!(v, JsonValue::Object(_) | JsonValue::Array(_))
                   {
                     let _ = Channel::from_callback_fn(webview, callback).send(v);
